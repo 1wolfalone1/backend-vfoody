b56d71660a615b63f8225cf6dd212f28b17f3131
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2024-05-25 21:07:58 +0700

    Created promotions selection queries

---
+++ VFoody.Infrastructure/Common/Queries/SelectPlatformPromotions.sql
@@ -0,0 +1,70 @@
+﻿/*
+    CreatedBy: DucDMD
+    Date: 21/05/2024
+
+    @PageIndex int
+    @PageSize int
+    @ShopId int
+    @SearchText string
+*/
+-- SET @PageIndex = 1;
+-- SET @PageSize = 12;
+-- SET @ShopId = 1; -- ID of the shop to filter products
+-- SET @SearchText = ''; -- Search keyword
+
+WITH ProductSearch AS (
+    SELECT
+        p.id AS product_id,
+        p.name AS product_name,
+        p.description AS product_description,
+        p.price AS product_price,
+        p.image_url AS product_image_url,
+        p.total_order AS product_total_order,
+        p.status AS product_status,
+        p.shop_id AS product_shop_id,
+        s.name AS shop_name,
+        s.logo_url AS shop_logo_url,
+        s.active AS shop_active,
+        s.active_from AS shop_active_from,
+        s.active_to AS shop_active_to,
+        ROW_NUMBER() OVER (
+            ORDER BY 
+                CASE 
+                    WHEN p.name LIKE CONCAT('%', @SearchText, '%') THEN 0 
+                    ELSE 1 
+                END, -- Priority to products with SearchText in name
+                p.total_order DESC
+        ) AS RowNum,
+        COUNT(p.id) OVER () AS TotalItems
+    FROM
+        v_foody.product p
+    INNER JOIN
+        v_foody.shop s ON p.shop_id = s.id
+    WHERE
+        p.status = true
+        AND s.status = true
+        AND p.shop_id = @ShopId
+)
+
+SELECT
+    product_id AS Id,
+    product_name AS Name,
+    product_description AS Description,
+    product_price AS Price,
+    product_image_url AS ImageUrl,
+    product_total_order AS TotalOrder,
+    product_status AS Status,
+    product_shop_id AS ShopId,
+    shop_name AS ShopName,
+    shop_logo_url AS ShopLogoUrl,
+    shop_active AS ShopActive,
+    shop_active_from AS ShopActiveFrom,
+    shop_active_to AS ShopActiveTo,
+    TotalItems,
+    CEILING(TotalItems * 1.0 / @PageSize) AS TotalPages
+FROM
+    ProductSearch
+WHERE
+    RowNum BETWEEN (@PageIndex - 1) * @PageSize + 1 AND @PageIndex * @PageSize
+ORDER BY
+    RowNum ASC;
---
+++ VFoody.Infrastructure/Common/Queries/SelectUserPromotions.sql
@@ -0,0 +1,78 @@
+﻿/*
+    CreatedBy: DucDMD
+    Date: 25/05/2024
+
+    @Email string
+    @PageIndex int
+    @PageSize int
+    @Status int
+    @StartDate DATETIME
+    @EndDate DATETIME
+    @Available boolean    
+*/
+-- SET @PageIndex = 1;
+-- SET @PageSize = 12;
+-- SET @Status = 1; -- 1 : active ; 2: locked ; 3: cancelled/deleted
+-- SET @StartDate = NULL;
+-- SET @EndDate = NULL;
+-- SET @Available = TRUE; -- number_of_used < usage_limit or not
+-- SET @Email = 'tienhoangbmt2911@gmail.com';
+
+WITH FilteredPersonPromotions AS (
+    SELECT
+        p.id,
+        p.amount_rate,
+        p.minimum_order_value,
+        p.maximum_apply_value,
+        p.amount_value,
+        p.apply_type,
+        p.start_date,
+        p.end_date,
+        p.usage_limit,
+        p.number_of_used,
+        p.created_date,
+        p.updated_date,
+        p.status,
+        ROW_NUMBER() OVER (
+            ORDER BY
+                CASE WHEN p.apply_type = 1 THEN 0 ELSE 1 END,
+                CASE WHEN p.apply_type = 1 THEN p.amount_rate ELSE p.amount_value END DESC
+        ) AS RowNum,
+        COUNT(p.id) OVER () AS TotalItems
+    FROM
+        person_promotion p
+        INNER JOIN account a ON p.account_id = a.id
+    WHERE
+        CURTIME() BETWEEN p.start_date AND p.end_date
+        AND p.usage_limit > p.number_of_used
+        AND (@Status = 0 OR p.status = @Status)
+        AND (@StartDate IS NULL OR p.start_date >= @StartDate)
+        AND (@EndDate IS NULL OR p.start_date <= @EndDate)
+        AND (@Available = FALSE OR p.number_of_used < p.usage_limit)
+        AND (@Email IS NULL OR a.email = @Email)
+)
+
+SELECT
+    id AS Id,    
+    start_date AS StartDate,
+    end_date AS EndDate,
+    
+    apply_type AS ApplyType,
+    amount_rate AS AmountRate,
+    amount_value AS AmountValue,
+    minimum_order_value AS MinimumOrderValue,
+    maximum_apply_value AS MaximumApplyValue,
+
+    usage_limit AS UsageLimit,
+    number_of_used AS NumberOfUsed,
+    created_date AS CreatedDate,
+    updated_date AS UpdatedDate,
+    status AS Status,
+    TotalItems,
+    CEILING(TotalItems * 1.0 / @PageSize) AS TotalPages
+FROM
+    FilteredPersonPromotions
+WHERE
+    RowNum BETWEEN (@PageIndex - 1) * @PageSize + 1 AND @PageIndex * @PageSize
+ORDER BY
+    RowNum ASC;
