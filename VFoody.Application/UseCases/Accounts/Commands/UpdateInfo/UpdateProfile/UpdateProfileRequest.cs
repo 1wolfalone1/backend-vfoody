a6871f6d133af3444780613360646a98260810b9
vanthong <vanthong07012002@gmail.com>
2024-06-08 00:38:27 +0700

    Feat: [ThongNV] Create api update customer infor and customer avatar

---
+++ VFoody.Application/UseCases/Accounts/Commands/UpdateInfo/UpdateProfile/UpdateProfileRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace VFoody.Application.UseCases.Accounts.Commands.UpdateInfo.UpdateProfile;
+
+public class UpdateProfileRequest
+{
+    public string PhoneNumber { get; set; }
+    public string FullName { get; set; }
+}
---
+++ VFoody.Application/UseCases/Accounts/Commands/UpdateInfo/UpdateProfile/UpdateProfileValidate.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace VFoody.Application.UseCases.Accounts.Commands.UpdateInfo.UpdateProfile;
+
+public class UpdateProfileValidate : AbstractValidator<UpdateProfileRequest>
+{
+    public UpdateProfileValidate()
+    {
+        RuleFor(a => a.PhoneNumber)
+            .NotEmpty()
+            .WithMessage("Số điện thoại là bắt buộc");
+        RuleFor(a => a.FullName)
+            .NotEmpty()
+            .WithMessage("Email là bắt buộc");
+    }
+}
