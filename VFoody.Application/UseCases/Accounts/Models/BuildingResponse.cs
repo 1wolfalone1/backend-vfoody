dd3301131987428bf913e3497b8ac975d4c158bf
vanthong <vanthong07012002@gmail.com>
2024-06-12 18:12:12 +0700

    Fix: Add Latitude Longitude to building entity

--- VFoody.Application/UseCases/Accounts/Models/BuildingResponse.cs
+++ VFoody.Application/UseCases/Accounts/Models/BuildingResponse.cs
@@ -4,4 +4,6 @@ public class BuildingResponse
 {
     public int Id { get; set; }
     public string Address { get; set; }
+    public float Latitude { get; set; }
+    public float Longitude { get; set; }
 }
