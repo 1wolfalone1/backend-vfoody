2a4b6aeab28d1e1c9a9f6791cf6c289f85dce3f0
Hoang Tien <hoangtienbmt2911@gmail.com>
2024-06-21 16:53:33 +0700

    [TienPH] Create favourite shop

--- VFoody.Application/UseCases/Shop/Queries/ListShop/GetListShopHandler.cs
+++ VFoody.Application/UseCases/Shop/Queries/ListShop/GetListShopHandler.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
-using Org.BouncyCastle.Utilities;
 using VFoody.Application.Common.Abstractions.Messaging;
+using VFoody.Application.Common.Repositories;
+using VFoody.Application.Common.Services;
 using VFoody.Application.Common.Services.Dapper;
 using VFoody.Application.UseCases.Shop.Models;
 using VFoody.Domain.Shared;
@@ -10,30 +11,42 @@ namespace VFoody.Application.UseCases.Shop.Queries.ListShop;
 public class GetListShopHandler : IQueryHandler<GetListShopQuery, Result>
 {
     private readonly IDapperService _dapperService;
+    private readonly IFavouriteShopRepository _favouriteShopRepository;
+    private readonly ICurrentPrincipalService _currentPrincipalService;
     private readonly ILogger<GetListShopHandler> _logger;
-    
 
-    public GetListShopHandler(IDapperService dapperService, ILogger<GetListShopHandler> logger)
+
+    public GetListShopHandler(IDapperService dapperService, ILogger<GetListShopHandler> logger,
+        IFavouriteShopRepository favouriteShopRepository, ICurrentPrincipalService currentPrincipalService
+    )
     {
         _dapperService = dapperService;
         _logger = logger;
+        _favouriteShopRepository = favouriteShopRepository;
+        _currentPrincipalService = currentPrincipalService;
     }
 
     public async Task<Result<Result>> Handle(GetListShopQuery request, CancellationToken cancellationToken)
     {
         try
         {
+            var accountId = _currentPrincipalService.CurrentPrincipalId!.Value;
             var list = await _dapperService.SelectAsync<SelectSimpleShopDTO>(QueryName.SelectShopByIds, new
             {
                 ShopIds = request.shopIds
             }).ConfigureAwait(false);
 
+            foreach (var item in list)
+            {
+                item.IsFavouriteShop = _favouriteShopRepository.IsFavouriteShop(item.Id, accountId);
+            }
             if (list != null && list.Count() > 0)
             {
                 return Result.Success(list);
             }
-            
-            return Result.Failure(new Error("404", $"Không tìm thấy danh sách shop với ids: {string.Join(", ", request.shopIds)}"));
+
+            return Result.Failure(new Error("404",
+                $"Không tìm thấy danh sách shop với ids: {string.Join(", ", request.shopIds)}"));
         }
         catch (Exception e)
         {
--- VFoody.Application/UseCases/Shop/Queries/ShopInfo/GetShopInfoHandler.cs
+++ VFoody.Application/UseCases/Shop/Queries/ShopInfo/GetShopInfoHandler.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using VFoody.Application.Common.Abstractions.Messaging;
 using VFoody.Application.Common.Repositories;
+using VFoody.Application.Common.Services;
 using VFoody.Application.UseCases.Shop.Models;
 using VFoody.Domain.Enums;
 using VFoody.Domain.Shared;
@@ -10,19 +11,29 @@ namespace VFoody.Application.UseCases.Shop.Queries.ShopInfo;
 public class GetShopInfoHandler : IQueryHandler<GetShopInfoQuery, Result>
 {
     private readonly IShopRepository _shopRepository;
+    private readonly IFavouriteShopRepository _favouriteShopRepository;
+    private readonly ICurrentPrincipalService _currentPrincipalService;
     private readonly IMapper _mapper;
 
-    public GetShopInfoHandler(IShopRepository shopRepository, IMapper mapper)
+    public GetShopInfoHandler(IShopRepository shopRepository, IMapper mapper,
+        IFavouriteShopRepository favouriteShopRepository, ICurrentPrincipalService currentPrincipalService
+    )
     {
         _shopRepository = shopRepository;
         _mapper = mapper;
+        _favouriteShopRepository = favouriteShopRepository;
+        _currentPrincipalService = currentPrincipalService;
     }
 
     public Task<Result<Result>> Handle(GetShopInfoQuery request, CancellationToken cancellationToken)
     {
-        var shop = _shopRepository.GetInfoByShopIdAndStatusIn(request.shopId, new int[]{(int)ShopStatus.Active});
+        var shop = _shopRepository.GetInfoByShopIdAndStatusIn(request.shopId, new int[] { (int)ShopStatus.Active });
+        var accountId = _currentPrincipalService.CurrentPrincipalId!.Value;
+        var isFavouriteShop = _favouriteShopRepository.IsFavouriteShop(request.shopId, accountId);
+        var shopInfoResponse = _mapper.Map<ShopInfoResponse>(shop);
+        shopInfoResponse.IsFavouriteShop = isFavouriteShop;
         return Task.FromResult<Result<Result>>(shop != null
-            ? Result.Success(_mapper.Map<ShopInfoResponse>(shop))
+            ? Result.Success(shopInfoResponse)
             : Result.Failure(new Error("400", "Not found this shop.")));
     }
 }
