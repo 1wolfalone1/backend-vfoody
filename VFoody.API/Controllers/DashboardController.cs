2e5a895a567f4cfd4f5b439c5c89c0e9e5e7b091
vanthong <vanthong07012002@gmail.com>
2024-07-05 00:29:41 +0700

    Fix: authorization of api dashboard

--- VFoody.API/Controllers/DashboardController.cs
+++ VFoody.API/Controllers/DashboardController.cs
@@ -10,22 +10,24 @@ namespace VFoody.API.Controllers;
 
 [ApiController]
 [Route("/api/v1/admin/dashboard")]
-[Authorize(Roles = IdentityConst.AdminClaimName)]
 public class DashboardController : BaseApiController
 {
     [HttpGet("overview")]
+    [Authorize(Roles = IdentityConst.AdminClaimName)]
     public async Task<IActionResult> GetOverviewAdminDashboard([FromQuery] GetDashboardAdminOverviewQuery query)
     {
         return this.HandleResult(await this.Mediator.Send(query));
     }
 
     [HttpGet("chart/order")]
+    [Authorize(Roles = IdentityConst.AdminClaimName)]
     public async Task<IActionResult> GetChartShowOrder([FromQuery] GetChartOrderAdminDashboardQuery query)
     {
         return this.HandleResult(await this.Mediator.Send(query).ConfigureAwait(false));
     }
     
     [HttpGet("chart/revenue")]
+    [Authorize(Roles = IdentityConst.AdminClaimName)]
     public async Task<IActionResult> GetChartShowRevenue([FromQuery] GetChartRevenueAdminDashboardQuery query)
     {
         return this.HandleResult(await this.Mediator.Send(query).ConfigureAwait(false));
