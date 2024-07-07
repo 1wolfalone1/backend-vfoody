bca51ae9760ef8b3a82eeb1bbc02a23d66bd7ad9
vanthong <vanthong07012002@gmail.com>
2024-07-07 22:58:26 +0700

    Feat: Create api for customer create feedback

---
+++ VFoody.Application/UseCases/Feedbacks/Models/CreateFeedbackResponse.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+using VFoody.Domain.Enums;
+
+namespace VFoody.Application.UseCases.Feedbacks.Models;
+
+public class CreateFeedbackResponse
+{
+    public int Id { get; set; }
+    public string Comment { get; set; }
+    public RatingRanges Rating { get; set; }
+    public string[] Images
+    {
+        get => this.ImagesUrl.Split(",");
+    }
+    public DateTime CreatedDate { get; set; }
+    
+    [JsonIgnore]
+    public string ImagesUrl { get; set; }
+}
